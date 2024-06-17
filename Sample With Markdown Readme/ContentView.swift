8842449314b1171b31383db870c40d76dd771f32
Stewart Lynch <slynch@createchsol.com>
2024-06-16 17:17:26 -0700

    Initial Commit

---
+++ Sample With Markdown Readme/ContentView.swift
@@ -0,0 +1,32 @@
+//
+// ----------------------------------------------
+// Original project: Sample With Markdown Readme
+// by  Stewart Lynch on 2024-06-16
+//
+// Follow me on Mastodon: @StewartLynch@iosdev.space
+// Follow me on Threads: @StewartLynch (https://www.threads.net)
+// Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+// ----------------------------------------------
+// Copyright © 2024 CreaTECH Solutions. All rights reserved.
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
